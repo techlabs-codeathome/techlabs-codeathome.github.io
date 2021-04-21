9e7e4495767e888dbf2660cce6d024add7a7b7ea
Codeathome GitHub Workflow <codeathome@techlabs.org>
2021-04-21 09:38:56 +0000

    Add frontend build at 2021-04-21T09:38:56UTC

--- plugins/vue-tippy.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import VueTippy, { TippyComponent } from 'vue-tippy'
-// Add additional themes.
-// import '@/assets/css/vue-tippy.scss'
-
-Vue.use(VueTippy)
-Vue.component('Tippy', TippyComponent)
