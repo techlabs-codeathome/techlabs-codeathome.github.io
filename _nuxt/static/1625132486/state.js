4656304c627fa39c7a07b84e2f1b06ccf58f1ff6
Codeathome GitHub Workflow <codeathome@techlabs.org>
2021-07-01 09:46:41 +0000

    Add frontend build at 2021-07-01T09:46:41UTC

--- _nuxt/static/1625132486/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625132486",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2b87b94a"}},globalRefs:{}}}(null,"\u002F"));
---
+++ _nuxt/static/1625132776/guide/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625132776",layout:"default",error:a,serverRendered:true,routePath:"\u002Fguide",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2082c883"}},globalRefs:{}}}(null));
